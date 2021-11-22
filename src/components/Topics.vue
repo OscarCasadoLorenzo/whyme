d74a6ab788fde7e8a87adaeb0c810552f9094e2a
oscar <ocl5@alu.ua.es>
2021-11-23 00:42:35 +0100

    Vue with Vuetify and Axios

---
+++ src/components/Topics.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-container>  
+        <v-card class= "mb-5" v-for="topic in topics" :key="topic._id">
+            <v-card-title>{{topic.title}}</v-card-title>
+            <v-card-text>{{topic.content}}</v-card-text>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+beforeMount(){
+    axios 
+      .get('http://localhost:3999/whyme/topics')
+      .then(response => {
+        this.topics = response.data.topics;
+      })
+  },
+
+  data (){
+    return {
+        topics : []
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
